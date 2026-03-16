2d2389362e225c8dbbaca51dfd3f2faac58162c3
Neeraj Giri <Neerajkumar04033@gmail.com>
2026-03-16 12:14:39 +0530

    first node express project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Page");
+});
+
+app.get("/api", (req, res) => {
+    res.json({
+        name: "Neeraj",
+        role: "Backend Learner",
+        language: "JavaScript"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
